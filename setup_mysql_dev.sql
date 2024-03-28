046fa40247c3976f263862cf85fb1ee77eb61b47
TuyenH <thtuyen199@gmail.com>
2024-03-28 13:52:32 +0000

    add flush privileges

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,3 +7,4 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- Set SELECT privilege on the database performance_schema for hbnb_dev:
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,3 +7,4 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 -- Set SELECT privilege on the database performance_schema for hbnb_test:
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
