1c7145b4e5abd45723276dba8829fbacc48a416c
TuyenH <thtuyen199@gmail.com>
2024-04-09 19:40:49 +0000

    Task 8- display list of states with sorting

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,17 @@
+#!usr/bin/python3
+"""Start a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models import State
+
+app = Flask(__name__, template_folder="templates")
+
+
+@app.route("/states_list")
+def list_state():
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=sorted(states, key=lambda s: s.name, reverse=False))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
